2123e91be4d9c7a8bb98cde234c6741fdeccd03a
SpReeDateD <webmaster@born2skate.de>
2021-09-27 10:55:29 +0200

    switch tests to nunit

--- Id3.Net.Tests/DebuggingTests.cs
+++ Id3.Net.Tests/DebuggingTests.cs
@@ -1,40 +1,23 @@
-#region --- License & Copyright Notice ---
-/*
-Copyright (c) 2005-2018 Jeevan James
-All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-#endregion
-
 using System;
 using System.IO;
 using Id3.Frames;
-using Xunit;
+using NUnit.Framework;
 
 namespace Id3.Net.Tests
 {
-    public sealed class DebuggingTests : IDisposable
+    [TestFixture]
+    public sealed class DebuggingTests
     {
-        private readonly Mp3 _mp3;
+        private Mp3 _mp3;
 
-        public DebuggingTests()
+        [SetUp]
+        public void SetUp()
         {
             var stream = new MemoryStream();
-            _mp3 = new Mp3(stream, Mp3Permissions.ReadWrite);
+            this._mp3 = new Mp3(stream, Mp3Permissions.ReadWrite);
         }
 
-        [Fact]
+        [Test]
         public void DebugTest()
         {
             var tag1 = new Id3Tag {
@@ -44,14 +27,15 @@ namespace Id3.Net.Tests
 
             Id3Tag tag2 = _mp3.GetTag(Id3Version.V23);
             tag2.Track.Padding = 4;
-            Assert.Equal(Id3Version.V23, tag2.Version);
-            Assert.Equal(Id3TagFamily.Version2X, tag2.Family);
-            Assert.Equal("0003/0010", tag2.Track);
+            Assert.AreEqual(Id3Version.V23, tag2.Version);
+            Assert.AreEqual(Id3TagFamily.Version2X, tag2.Family);
+            Assert.AreEqual("0003/0010", tag2.Track.TextValue);
         }
 
-        void IDisposable.Dispose()
+        [TearDown]
+        public void TearDown()
         {
-            _mp3.Dispose();
+            this._mp3?.Dispose();
         }
     }
 }
